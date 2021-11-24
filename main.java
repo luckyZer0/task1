a06c53e45840c0f775f037db4c02285063eea982
luckyZer0 <luckymezerg@gmail.com>
2021-11-24 13:45:29 +0800

    added new variables in java

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         String pet1 = "Cat";
         String pet2 = "Kurma";
-        System.out.println("My " + pet1 + " name is " + pet2);
+        String pet3 = "Kurama";
+        System.out.println("My " + pet1 + " name is " + pet2 + " and mu dog name is " + pet3);
     }
 }
